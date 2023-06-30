922fc1463eb7c7e9768ff6812600d213ea6f52b9
ZheerBarzan <zheerbarzan85@outlook.com>
2023-06-30 18:15:41 +0300

    first commit

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckbox = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("learn flutter"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/rick.gif'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.blue.shade400,
+            width: double.infinity,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            child: const Center(
+              child: Text(
+                "never gonna give you up",
+                style: TextStyle(color: Color.fromARGB(255, 214, 68, 92)),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: isSwitch
+                  ? const Color.fromARGB(255, 0, 188, 212)
+                  : const Color.fromARGB(255, 81, 168, 84),
+            ),
+            onPressed: () {},
+            child: const Text("eleveted button"),
+          ),
+          OutlinedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: isSwitch
+                  ? const Color.fromARGB(255, 0, 188, 212)
+                  : const Color.fromARGB(255, 81, 168, 84),
+            ),
+            onPressed: () {},
+            child: const Text("outlined button"),
+          ),
+          TextButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: isSwitch
+                  ? const Color.fromARGB(255, 0, 188, 212)
+                  : const Color.fromARGB(255, 81, 168, 84),
+            ),
+            onPressed: () {},
+            child: const Text("text button"),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.apple,
+                color: Color.fromARGB(255, 101, 104, 105),
+              ),
+              Icon(
+                Icons.android,
+                color: Color.fromARGB(255, 114, 190, 117),
+              ),
+              Icon(
+                Icons.window_sharp,
+                color: Color.fromARGB(255, 80, 149, 205),
+              ),
+            ],
+          ),
+          Switch(
+            value: isSwitch,
+            onChanged: (bool newBool) {
+              setState(() {
+                isSwitch = newBool;
+              });
+            },
+          ),
+          Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckbox = newBool;
+                });
+              }),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const HomePage(),
+      appBar: AppBar(
+        title: const Text("XYZ"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_sharp),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "profile"),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
