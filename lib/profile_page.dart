5838633ca4c70c9ee45a39d22d6978f709f2af67
ZheerBarzan <zheerbarzan85@outlook.com>
2023-07-02 18:37:11 +0300

    the end of the project

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+const int listItems = 20;
+
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: listItems,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text("list item ${(index + 1)} "),
+            leading: const Icon(Icons.person),
+            trailing: const Icon(Icons.check_box),
+            onTap: () {
+              debugPrint("list item ${(index + 1)} selected ");
+            },
+          );
+        });
   }
 }
